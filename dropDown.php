a6ec67ee79eca4c17747e974d40bc454a529ccd0
chipset1 <stephenaemblem@gmail.com>
2014-11-21 16:41:25 -0500

    Revert "Rough Code of CurrentPlanTable"
    This reverts commit d8cadea49e8a99f2e31310f538df2bdf4c486890.

--- dropDown.php
+++ dropDown.php
@@ -1,11 +1,15 @@
 <?php
 include("connect.php");
 
-$sql = "SELECT distinct pgmName from studentstats";
+//for new database
+$sql = "SELECT distinct program_name from program order by program_name";
+
+//for fat table
+//$sql = "select distinct pgmName from studentstats order by pgmName";
+
 $result = mysqli_query($db,$sql);
 while ( $row = mysqli_fetch_array($result))
 {
 	echo "<li><a role=\"menuitem\" href=\"javascript:void(0)\" value=\"$row[0]\">$row[0]</a></li>";
 }
-
 ?>
--- selectGrades.php
+++ selectGrades.php
@@ -1,21 +1,45 @@
 <?php
-	include("connect.php");
+include("connect.php");
 
-	$name = $_GET['studentName'];
 // 'Caterina Roberts'
+
 	// $error = $name;
-	$level = $_GET['level'];
+$name = $_GET['studentName'];
+$level = $_GET['level'];
 	// add A to level
+	
+	//with sproject database
+	//$result = mysqli_query($db,"SELECT courseNumber, courseName, grade , aLevel
+	//						from studentStats WHERE studentName = '$name' AND aLevel = 'A$level' AND grade != '' ORDER BY aLevel ");
 
-	$result = mysqli_query($db,"SELECT courseNumber, courseName, grade , aLevel
-							from studentStats WHERE studentName = '$name' AND aLevel = 'A$level' AND grade != '' ORDER BY aLevel ");
-
+	//with softwareproject database (new)
+	// if ($level == null){
+	// 	$result = mysqli_query($db, "select e.course_no, c.course_name, e.grade, e.a_level
+	// 						from student_enrollment e, course c, student s
+	// 						where e.course_no = c.course_no
+	// 						and e.student_no = s.student_no
+	// 						and s.student_name = '$name'
+	// 						and e.grade != ''
+	// 						order by e.a_level");
+	// }
+	// else{
+		$result = mysqli_query($db, "select e.course_no, c.course_name, e.grade, e.a_level
+							from student_enrollment e, course c, student s
+							where e.course_no = c.course_no
+							and e.student_no = s.student_no
+							and s.student_name = '$name'
+							and e.grade != ''
+							order by e.a_level");
+							//and e.a_level = '$level' //there is no a_level = A1, so exclude from the query for now
+	// }
+	
 	$rows = array();
 
-	while($r = mysqli_fetch_array($result)) {
-	    array_push($rows, $r);
-	}
 
-	header('Content-type: application/json');
-	echo json_encode($rows); 
+while($r = mysqli_fetch_array($result)) {
+    array_push($rows, $r);
+}
+
+header('Content-type: application/json');
+echo json_encode($rows); 
 ?>
