d97f8e23d81608f52ee75956f558856b56b0c315
chipset1 <stephenaemblem@gmail.com>
2014-11-22 10:22:45 -0500

    testing

--- dropDownALevel.php
+++ dropDownALevel.php
@@ -1,18 +1,7 @@
 <?php
 include("connect.php");
-/**
-$program = $_GET['name'];
 
-if (!empty($_GET['name'])){
-	$sql = "select distinct e.a_level 
-			from student_enrollment e, program p
-			where e.program_no = p.program_no
-			and p.program_name = '$program'
-			order by e.a_level";
-} else {
-	$sql = "SELECT distinct a_level from student_enrollment order by a_level";
-}
-**/
+
 
 
 $sql = "SELECT distinct a_level from student_enrollment order by a_level";
