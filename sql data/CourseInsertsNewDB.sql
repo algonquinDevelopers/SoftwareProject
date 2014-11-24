2b4f0cde7059766f8911baba4341124c78c93fd1
chipset1 <stephenaemblem@gmail.com>
2014-11-24 11:50:11 -0500

    code clean up
    also rename files to be consistent.

--- sql data/CourseInsertsNewDB.sql
+++ sql data/CourseInsertsNewDB.sql
@@ -13,6 +13,22 @@ CON8404 	Civil Estimating
 ENG8102 	Strength of Materials
 ENG8454 	Geotechnical Materials
 
+INSERT INTO `course`(`course_no`, `course_level`, `course_name`) 
+VALUES ('CAD8400', '03', 'AutoCAD I');
+
+INSERT INTO `program_course` (`program_no`, `program_version`, `department_no`, `course_no`)
+VALUES ('0190W', '01/09/2011', 'ARCI', 'CAD8400');
+
+INSERT INTO `course`(`course_no`, `course_level`, `course_name`) 
+VALUES ('CON8404', '03', 'Civil Estimating');
+
+INSERT INTO `course`(`course_no`, `course_level`, `course_name`) 
+VALUES ('ENG8102', '03', 'Strength of Materials');
+
+
+INSERT INTO `course`(`course_no`, `course_level`, `course_name`) 
+VALUES ('ENG8454', '03', 'Geotechnical Materials');
+
 level 4
 CON8476 	Business Principles
 ENL2019T 	Technical Communication for Engineering Technologies
